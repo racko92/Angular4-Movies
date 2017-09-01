6c1ecb1c4a53536b4d6ab3184e70b1dfbffcfbf2
racko92 <vladimir.rackovic.92@gmail.com>
2017-09-01 15:05:08 +0200

    Zadatak 1: Napravljen projekat, kreiran app-router, importovan Bootstrap, kreirana MoviesComponent-a, uradjena redirekcija sa "/" na "/movies"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
